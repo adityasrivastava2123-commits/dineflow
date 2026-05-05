38c390338ce2c507fbb15f779e15903cad5f164c
unknown <adityasrivastava2123@gmail.com>
2026-05-05 23:14:40 +0530

    fix cors allow all

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST'],
   },
 });
@@ -32,13 +32,7 @@ console.log({
 });
 
 // Middleware
-app.use(cors({ 
-  origin: [
-    process.env.CLIENT_URL,
-    'http://localhost:5173',
-  ],
-  credentials: true
-}));
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
